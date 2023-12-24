422589e7f2ab1e5431c7db06149202bf974bba9a
shell-thon <shellterminal0@gmail.com>
2023-12-24 11:58:25 +0100

    update

--- backend/index.js
+++ backend/index.js
@@ -20,43 +20,43 @@ app.get('/', (request, response) => {
 app.post('/books', async (request, responds) => {
 
     try {
-	if (
+        if (
             !request.body.title ||
-	    !request.body.author ||
-	    !request.body.publishYear
-	) {
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
             return response.status(400).send({
-		message: ' send all required fields; title, author, publishYear',
-	    });
-	}
-	const newBook = {
-	    title: request.body.title,
-	    author: request.body.author,
-	    publisherYear: request.body.publisherYear,
-	};
+                message: ' send all required fields; title, author, publishYear',
+            });
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publisherYear: request.body.publisherYear,
+        };
 
-	const book = await Book.create(newBook);
+        const book = await Book.create(newBook);
 
-	return response.status(201).send(book);
-	
+        return response.status(201).send(book);
+
     } catch (error) {
-      console.log(error.message);
-      response.status(500).send({ message: error.message });
-    } 
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
 });
 
 //Route for Get All Books from Database
 app.get('/books', async (request, response) => {
     try {
-	const books = await Book.find({});
+        const books = await Book.find({});
 
-	return response.status(200).json({
+        return response.status(200).json({
             count: books.length,
             data: books
-	});
-    }   catch (error) {
-	console.log(error.message);
-	response.status(500).send({ message: error.message });
+        });
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
     }
 });
 
@@ -64,12 +64,12 @@ app.get('/books', async (request, response) => {
 app.get('/books/:id', async (request, response) => {
     try {
 
-	const { id } = request.params;
-	
-        const book = await Book.findById({id});
+        const { id } = request.params;
+
+        const book = await Book.findById({ id });
 
         return response.status(200).json(book);
-    }   catch (error) {
+    } catch (error) {
         console.log(error.message);
         response.status(500).send({ message: error.message });
     }
@@ -81,32 +81,32 @@ app.put('/books/:id', async (request, response) => {
         if (
             !request.body.title ||
             !request.body.author ||
-	    !request.body.publishYear
-	) {
+            !request.body.publishYear
+        ) {
             return response.status(400).send({
                 message: ' send all required fields; title, author, publishYear',
-	    });
-	}
+            });
+        }
 
-	const { id } = request.params
+        const { id } = request.params
 
-	const result = await Book.findByIdAndUpdate();
+        const result = await Book.findByIdAndUpdate();
 
 
-    }catch (error) {
-	 console.log(error.message);
-	 response.status(500).send({ message: error.message });
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
     }
 });
 
 mongoose
     .connect(mongoDBURL)
     .then(() => {
-	console.log('App connected to Database');
-	app.listen(PORT, () => {
-	    console.log(`App is listening to port: ${PORT}`);
-	});
+        console.log('App connected to Database');
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`);
+        });
     })
     .catch((error) => {
-	console.log(error);
+        console.log(error);
     });
--- backend/models/bookModel.js
+++ backend/models/bookModel.js
@@ -1,23 +1,23 @@
 import mongoose from 'mongoose';
 
-const bookSchema = mongoose.Schema (
-    {
-	title: {
-	    type: String,
-	    required: true,
+const bookSchema = mongoose.Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		author: {
+			type: String,
+			required: true,
+		},
+		publishYear: {
+			type: Number,
+			required: true,
+		},
 	},
-	author: {
-	    type: String,
-	    required: true,
-	},
-	publishYear: {
-	    type: Number,
-	    required: true,
-	},
-    },
-    {
-	timestamps: true,
-    }
+	{
+		timestamps: true,
+	}
 );
 
 export const Book = mongoose.model('Cat', bookSchema);
