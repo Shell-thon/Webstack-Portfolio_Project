a80cefed7c6aa085b28005db5fd387ed70b7f730
Shell-thon <shellterminal0@gmail.com>
2023-12-26 12:44:17 +0100

    update

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+// Update the MongoDB connection string with SSL disabled
+export const mongoDBURL = 'mongodb+srv://root:root@book-store-mernstack.j1oqshs.mongodb.net/?retryWrites=true&w=majority&ssl=false';
