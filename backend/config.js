2008ceabdd999e5f60cf4fe204f37dbea014fcc9
Shell-thon <shellterminal0@gmail.com>
2024-01-10 02:43:24 +0100

    update fix with atlas connection

--- backend/config.js
+++
@@ -1,4 +0,0 @@
-export const PORT = 5555;
-
-// Update the MongoDB connection string with SSL disabled
-export const mongoDBURL = 'mongodb+srv://root:root@book-store-mernstack.j1oqshs.mongodb.net/?retryWrites=true&w=majority';
